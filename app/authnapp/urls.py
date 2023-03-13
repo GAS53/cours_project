2368205b97502675fe2b4bf1d356dfdf25672997
12boris <85038312+12boris@users.noreply.github.com>
2023-03-14 02:19:01 +0300

    Update urls.py

--- app/authnapp/urls.py
+++ app/authnapp/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
-
+from django.urls import path, include
 import authnapp.views as authnapp
+from rest_framework.routers import DefaultRouter
+from .views import ShopUserModelViewSet
 
 app_name = "authnapp"
+
+router = DefaultRouter()
+router.register('Users', ShopUserModelViewSet)
 
 urlpatterns = [
     path("login/", authnapp.login, name="login"),
     path("logout/", authnapp.logout, name="logout"),
     path("register/", authnapp.register, name="register"),
     path("edit/", authnapp.edit, name="edit"),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
