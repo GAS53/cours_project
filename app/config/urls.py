36c65ab6d01acbd043e93bbe96d1a72b89632b8d
12boris <85038312+12boris@users.noreply.github.com>
2023-03-14 17:47:43 +0300

    Update urls.py

--- app/config/urls.py
+++ app/config/urls.py
@@ -9,8 +9,8 @@ app_name = 'config'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", RedirectView.as_view(url="mainapp/")),
-    path("mainapp/", include("mainapp.urls", namespace='mainapp')),
+    path("mainapp", include("mainapp.urls", namespace="main")),
     path("authn/", include("authnapp.urls", namespace='auth')),
     path("apiapp/", include("apiapp.urls", namespace='apiapp')),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
 ]
