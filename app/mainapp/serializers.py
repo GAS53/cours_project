379c9b1713b26ee55dcce4ba1d10981a372d8d08
12boris <85038312+12boris@users.noreply.github.com>
2023-03-14 20:39:28 +0300

    Delete app/mainapp directory

--- app/mainapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Idea
-
-
-class IdeaModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = Idea
-        fields = '__all__'
