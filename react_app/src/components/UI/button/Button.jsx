70d7788c6f52e1681db5aec8f80c9d7937eb4883
gas53 <gas53@bk.ru>
2023-04-01 10:43:26 +0300

    clean stageg changes

--- react_app/src/components/UI/button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Button = ({type, text, styles, action}) => {
-    return (
-        <button type={type} className={["btn", styles].join(' ')} onClick={action}>
-            {text}
-        </button>
-    );
-};
