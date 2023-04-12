d6c3b4e6c1d952390c221e0220a753317878a795
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2023-04-12 11:58:39 +0300

    В скрипт заполнения БД добалено заполнение рубрик

--- app/backend/management/commands/fill_db.py
+++ app/backend/management/commands/fill_db.py
@@ -1,15 +1,22 @@
 from django.core.management import BaseCommand
 
 from authapp.models import BaseIdeinerUser
+from backend.models import Rubric
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         # Создаем пользователей
         for i in range(1, 4):
-            if not BaseIdeinerUser.objects.filter(login=f'test{i}@test.com'):
+            if not BaseIdeinerUser.objects.filter(email=f'test{i}@test.com'):
                 BaseIdeinerUser.objects.create_user(login=f'login{i}',
                                                     email=f'test{i}@test.com',
                                                     first_name=f'name{i}',
                                                     last_name=f'name{i}',
                                                     password='1234')
+
+        # Создаем рубрики
+        rubrics = ('Python', 'Javascript')
+        for rubric in rubrics:
+            if not Rubric.objects.filter(rubirc_name=rubric).first():
+                Rubric.objects.create(rubirc_name=rubric)
