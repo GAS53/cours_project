de56ca43618c5d0392a0a456bd3795578e26cef0
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2023-04-12 11:52:18 +0300

    Добавлен скрипт заполнения БД пользователями

---
+++ app/backend/management/commands/fill_db.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+
+from authapp.models import BaseIdeinerUser
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # Создаем пользователей
+        for i in range(1, 4):
+            if not BaseIdeinerUser.objects.filter(login=f'test{i}@test.com'):
+                BaseIdeinerUser.objects.create_user(login=f'login{i}',
+                                                    email=f'test{i}@test.com',
+                                                    first_name=f'name{i}',
+                                                    last_name=f'name{i}',
+                                                    password='1234')
