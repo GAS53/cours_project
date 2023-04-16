e61d224f5f023bb386ba452af6d2c6a966b866ad
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2023-04-16 14:08:44 +0300

    В админку добавлена возможность манипулирования всеми данными БД

--- app/authapp/admin.py
+++ app/authapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from authapp.models import BaseIdeinerUser
+
 # Register your models here.
+admin.site.register(BaseIdeinerUser)
--- app/backend/admin.py
+++ app/backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from backend.models import Rubric, Idea, Feedback, JoinedUser, LikesToIdea
+
 # Register your models here.
+admin.site.register(Rubric)
+admin.site.register(Idea)
+admin.site.register(Feedback)
+admin.site.register(JoinedUser)
+admin.site.register(LikesToIdea)
